c8b8f7cf0b2ca20342774b6e0989e39cfa4312c4
Alexandra Arévalo <108038316+alexa-arevalo@users.noreply.github.com>
2023-11-16 20:11:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #fb4993;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #000 ;">Estudio la carrera de Ingeniería Biomédica en la Facultad de Ingeneria Mecánica y Eléctrica.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
